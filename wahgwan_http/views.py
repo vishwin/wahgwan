da59872d15a8be4c91ed2d620d12a9840548ed86
Charlie Li <git@vishwin.info>
2017-03-22 03:43:23 -0400

    Include Font Awesome
    - submodule Font Awesome
    - import Font Awesome in app.scss
    - create route for Font Awesome font glyphs
    - clean up pkg_resources.resource_filename()

--- wahgwan_http/views.py
+++ wahgwan_http/views.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from flask import render_template, Response, abort
+from flask import render_template, Response, abort, send_file
 from flask_libsass import *
 import pkg_resources
 from slimit import minify
@@ -12,16 +12,20 @@ Sass(
 	{'app': 'scss/app.scss'},
 	app,
 	url_path='/static/css',
-	include_paths=[pkg_resources.resource_filename('wahgwan_http.views', 'scss')],
+	include_paths=[pkg_resources.resource_filename('wahgwan_http', 'scss')],
 	output_style='compressed'
 )
+
+@app.route('/static/fonts/<fontfile>')
+def fontawesome_file(fontfile):
+	return send_file(pkg_resources.resource_filename('wahgwan_http', 'Font-Awesome/fonts/' + fontfile))
 
 @app.route('/static/js/<js>')
 def render_js(js):
 	try:
 		out=cache.get(js)
 		if out is None:
-			fd=open(pkg_resources.resource_filename('wahgwan_http.views', 'js/' + js), encoding='UTF-8')
+			fd=open(pkg_resources.resource_filename('wahgwan_http', 'js/' + js), encoding='UTF-8')
 			out=minify(fd.read(), mangle=True, mangle_toplevel=True)
 			fd.close()
 			cache.set(js, out)
